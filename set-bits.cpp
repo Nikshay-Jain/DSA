a8bc2a6c8ee683f90417d81b3a352fed5d6334cc
Nikshay Jain <nikshay.p.jain@gmail.com>
2023-06-14 23:06:59 +0530

    set-bits

--- set-bits.cpp
+++ set-bits.cpp
@@ -27,20 +27,20 @@ void brian_kerningam(int n)
 }
 
 //O(n) = 1
-void lookup_table(int n)
+void lookup_table(int n)            //for 32 bit nos only. In case of 64 bit nos, extend the repetition more no of times
 {
     int table[256],i;
     table[0] = 0;
     for(i=1;i<256;i++)
         table[i] = (i&1) + table[i/2];
     
-    int c=table[n&0xff];
+    int c=table[n & 0xff];          //initialises c with bitwise AND of n and no with 8 1's
+    n=n>>8;                         //shifts n by 8 bits to right
+    c+=table[n & 0xff];             //adds c with bitwise AND of new n and no with 8 1's
     n=n>>8;
-    c+=table[n&0xff];
+    c+=table[n & 0xff];
     n=n>>8;
-    c+=table[n&0xff];
-    n=n>>8;
-    c+=table[n&0xff];
+    c+=table[n & 0xff];
     cout<<"The no has "<<c<<" bits set by look-up table algo"<<endl;
 }
 
