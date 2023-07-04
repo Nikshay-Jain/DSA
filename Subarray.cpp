309d86742ce4f15af8b87c37d038e8667431e6b6
Nikshay Jain <nikshay.p.jain@gmail.com>
2023-07-04 23:28:27 +0530

    Subarray.cpp

--- Subarray.cpp
+++ Subarray.cpp
@@ -1,15 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int subsum(int a[], int n, int sum)
+{
+    if(n==0)
+        return ((sum==0)?1:0);
+    
+    return subsum(a, n-1, sum) + subsum(a,n-1,sum-a[n-1]);
+}
+
 int main()
 {
-    int n,i,j,k;
+    int n,i,j,k,sum;
     cout<<"Enter size of array"<<endl;
     cin>>n;
     int a[n];
     cout<<"Enter array"<<endl;
     for(i=0;i<n;i++)
         cin>>a[i];
-    cout<<"Sum of subarrays:"<<endl;
+    cout<<"Enter sum needed"<<endl;
+    cin>>sum;
+    
+    cout<<"\nSubarrays:"<<endl;
     for(i=0;i<n;i++)
     {
         for(j=i;j<n;j++)
@@ -19,4 +31,5 @@ int main()
             cout<<endl;
         }
     }
+    cout<<subsum(a, n, sum);
 }
