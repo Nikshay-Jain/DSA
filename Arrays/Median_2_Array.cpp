f40cf07149b26a79155e9d5d650e496bd4133849
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-05-30 00:04:04 +0530

    Updated Arrays/Median_2_Array.cpp

--- Arrays/Median_2_Array.cpp
+++ Arrays/Median_2_Array.cpp
@@ -5,7 +5,7 @@ double med(int a1[], int a2[], int n1, int n2)
 {
     int beg1 = 0, end1 = n1, med = 0,i1,i2,min1,max1,min2,max2;
 
-    while(beg1<end1)
+    while(beg1<=end1)
     {
         i1 = (beg1 + end1)/2;
         i2 = ((n1+n2+1)/2) - i1;
@@ -26,22 +26,13 @@ double med(int a1[], int a2[], int n1, int n2)
         else
             beg1 = i1+1;
     }
+    return -1;
 }
 
 int main()
 {
-    int i,n1,n2;
-    cout<<"Enter no of elements in array 1"<<endl;
-    cin>>n1;
-    cout<<"Enter sorted array 1"<<endl;
-    int a1[n1];
-    for (i=0;i<n1;i++)
-        cin>>a1[i];
-    cout<<"Enter no of elements in array 2"<<endl;
-    cin>>n2;
-    cout<<"Enter sorted array 2"<<endl;
-    int a2[n2];
-    for (i=0;i<n2;i++)
-        cin>>a2[i];
+    int i,n1 = 5,n2 = 9;
+    int a1[n1] = {10,20,30,40,50};
+    int a2[n2] = {5,15,25,30,35,55,65,75,85};
     cout<<"Median of the 2 arrays combined is: "<<med(a1,a2,n1,n2)<<endl;
 }
