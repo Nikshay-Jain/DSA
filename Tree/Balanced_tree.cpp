96b4d3df950ea7b3a6dd8c341f805ed0d6fd5001
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-06-18 23:45:35 +0530

    Added Tree/

---
+++ Tree/Balanced_tree.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int isBalanced(Node *root)
+{
+    if(root==NULL)
+        return 0;
+    int lh = isBalanced(root->left);
+    if(lh == -1)
+        return -1;
+    int rh = isBalanced(root->right);
+    if(rh == -1)
+        return -1;
+    if(abs(lh-rh)>1)
+        return -1;
+    else
+        return 1+max(lh,rh);
+}
+
+int main()
+{
+    Node *root = new Node(20);
+    root->left = new Node(8);
+    root->right = new Node(12);
+    root->right->left =  new Node(3);
+    root->right->right = new Node(9);
+
+    cout<<isBalanced(root);
+}
