6444f10b0bc4853d9df8ccfe71f62df734c40e9e
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-06-26 18:37:19 +0530

    Updated Tree_Traversal.cpp

--- Tree/Tree_Traversal.cpp
+++ Tree/Tree_Traversal.cpp
@@ -39,7 +39,7 @@ int height(Node *root) {
     if(root==NULL)
         return 0;
     else
-        return max(height(root->left), height(root->right))+1;
+        return max(height(root->left), height(root->right)) + 1;
 }
 
 void nodes_at_k(Node *root, int k) {
