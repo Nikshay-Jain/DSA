e93317240f8535f0a055a7c7d66b650e3daf4ec5
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-06-26 11:40:09 +0530

    Updated Dequeue.cpp

--- Queue - Dequeue/Dequeue.cpp
+++ Queue - Dequeue/Dequeue.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 int main()
 {
-    deque<int> d = {10,20,30};
-    d.push_front(5);
-    d.push_back(50);
-    for(auto x: d)
-    cout<<x<<endl;
-    cout<<d.front()<<" "<<d.back()<<endl;
-    d.pop_front();
-    d.pop_back();
-    cout<<d.size();
+    deque<int> dq = {10,20,30};
+    dq.push_front(5);
+    dq.push_back(50);
+    for(auto x: dq)
+        cout<<x<<" ";
+    cout<<endl<<dq.front()<<" "<<dq.back()<<endl;
+    dq.pop_front();
+    dq.pop_back();
+    cout<<dq.size()<<endl;
+
+    auto it = dq.begin();
+    it++;
+    dq.insert(it, 67);      // to insert in between the dequeue
+    for(auto x: dq)
+        cout<<x<<" ";
 }
