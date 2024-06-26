33b8f3fd9ac677dbdad486b83e31d66150624caa
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-06-26 23:46:46 +0530

    Updated Max_width.cpp

--- Tree/Max_width.cpp
+++ Tree/Max_width.cpp
@@ -1,33 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+struct Node {
     int key;
-    Node *left;
-    Node *right;
-    Node(int k)
-    {
+    Node *left, *right;
+    Node(int k) {
         key = k;
         left = right = NULL;
     }
 };
 
-int max_width(Node *root)
-{
+int max_width(Node *root) {
     if(root==NULL)
         return 0;
     queue <Node*> q;
     q.push(root);
     int res = 0,i;
-    while(q.empty()==false)
-    {
+    
+    while(q.empty()==false) {
         int count = q.size();
         res = max(res,count);
-        for(i=0;i<count;i++)
-        {
+
+        for(i=0;i<count;i++) {
             Node *curr = q.front();
             q.pop();
+
             if(curr->left!=NULL)
                 q.push(curr->left);
             if(curr->right!=NULL)
@@ -37,8 +34,7 @@ int max_width(Node *root)
     return res;
 }
 
-int main()
-{
+int main() {
     Node *root = new Node(10);
     root->left = new Node(20);
     root->right = new Node(30);
