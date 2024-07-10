86b04b177907374ba208f7142802d50ee12c1f2b
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-07-10 22:44:52 +0530

    Updated Fractional_knapsack.cpp

--- Greedy/Fractional_knapsack.cpp
+++ Greedy/Fractional_knapsack.cpp
@@ -1,25 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mycmp(pair<int,int> a, pair<int,int> b)
-{
-    return (double(a.first/a.second) > double(b.first/b.second));
+int mycmp(pair<int,int> a, pair<int,int> b) {
+    return ((double)a.first/a.second > (double)b.first/b.second);
 }
 
-int fknap(pair<int,int> arr[], int n, int w)
-{
-    sort(arr,arr+n,mycmp);
+int fknap(vector<pair<int,int>> arr, int w) {
+    sort(arr.begin(),arr.end(),mycmp);
     double res = 0.0;
-    int i=0;
-    while(w>=0 && i<n)
-    {
-        if(w>=arr[i].second)
-        {
+    int i=0, n = arr.size();
+    while(w>=0 && i<n) {
+        if(w>=arr[i].second) {
             res+=arr[i].first;
             w-=arr[i].second;
         }
-        else
-        {
+        else {
             res+=((arr[i].first)*((double)w/arr[i].second));
             break;
         }
@@ -28,10 +23,8 @@ int fknap(pair<int,int> arr[], int n, int w)
     return res;
 }
 
-int main()
-{
-    int n = 3;
-    pair<int, int> arr[3] = {make_pair(120, 30), make_pair(100, 20), make_pair(60, 10)};
+int main() {
+    vector<pair<int, int>> arr = {make_pair(120, 30), make_pair(100, 20), make_pair(60, 10)};
     int w = 50;
-    cout<<"Max profit: "<<fknap(arr,n,w);
+    cout<<"Max profit: "<<fknap(arr,w);
 }
