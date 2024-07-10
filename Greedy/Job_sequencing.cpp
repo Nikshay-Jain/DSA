a0928ea3489577f5db0ea33642a9ce5ea372ea2a
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-07-10 22:44:52 +0530

    Updated Job_sequencing.cpp

--- Greedy/Job_sequencing.cpp
+++ Greedy/Job_sequencing.cpp
@@ -1,15 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mycmp(pair<int,int> a, pair<int,int> b)
-{
+int mycmp(pair<int,int> a, pair<int,int> b) {
     return (a.second > b.second);
 }
 
-int job(pair<int,int> arr[], int n)
-{
-    int i, summ = 0;
-    int maxDeadline = 0, sum=0;
+int job(vector<pair<int,int>> arr) {
+    int i, summ = 0, maxDeadline = 0, sum=0, n=arr.size();
     for (i=0;i<n;i++)
         maxDeadline = max(maxDeadline, arr[i].first);
     
@@ -17,12 +14,10 @@ int job(pair<int,int> arr[], int n)
     for(i=0;i<maxDeadline;i++)
         res[i] = 0;
     
-    sort(arr,arr+n,mycmp);
+    sort(arr.begin(),arr.end(),mycmp);
 
-    for(i=0;i<n;i++)
-    {
-        if(res[arr[i].first-1] == 0)
-        {
+    for(i=0;i<n;i++) {
+        if(res[arr[i].first-1] == 0) {
             res[arr[i].first-1] = arr[i].second;
             summ+=arr[i].second;
         }
@@ -30,9 +25,7 @@ int job(pair<int,int> arr[], int n)
     return summ;
 }
 
-int main()
-{
-    int n = 3;
-    pair<int,int> arr[5] = {make_pair(4,50), make_pair(1,5), make_pair(1,20), make_pair(5,10), make_pair(5,80)};
-    cout<<"Max profit: "<<job(arr,n)<<endl;
+int main() {
+    vector<pair<int,int>> arr = {make_pair(4,50), make_pair(1,5), make_pair(1,20), make_pair(5,10), make_pair(5,80)};
+    cout<<"Max profit: "<<job(arr)<<endl;
 }
