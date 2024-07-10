79430e1fc6c56d5d6a0dc6650fc8970cbb8b674e
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-07-10 22:44:52 +0530

    Updated Activity_selection.cpp

--- Greedy/Activity_selection.cpp
+++ Greedy/Activity_selection.cpp
@@ -1,22 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mycmp(pair<int,int> a, pair<int,int> b)
-{
+int mycmp(pair<int,int> a, pair<int,int> b) {
     return(a.second<b.second);
 }
 
 //Time: O(nlog(n))
-int myact(pair<int,int> arr[], int n)
-{
+int myact(pair<int,int> arr[], int n) {
     sort(arr,arr+n,mycmp);      //using personal comparator function to sort according to 2nd no in pair instead of default 1st
-    int prev = 0;
-    int res = 1;
-    int curr;
-    for(curr = 1; curr<n;curr++)
-    {
-        if(arr[curr].first >= arr[prev].second)     //check if start time of current activity is >= finish time of prev selected activity
-        {
+    int prev = 0, res = 1, curr;
+    for(curr=1; curr<n;curr++) {
+        if(arr[curr].first >= arr[prev].second) {    //check if start time of current activity is >= finish time of prev selected activity
             res++;
             prev=curr;
         }
@@ -24,8 +18,7 @@ int myact(pair<int,int> arr[], int n)
     return res;
 }
 
-int main()
-{
+int main() {
     int n = 3;
     pair<int, int> arr[3] = {make_pair(12, 25), make_pair(10, 20), make_pair(20, 30)};
     cout<<"No of activities: "<<myact(arr,n);
